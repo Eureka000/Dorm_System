b11b640bdc8d6f67d4b440811a5f3d0bcacc3295
zqy <greens_zhang@139.com>
2017-12-21 09:50:07 +0800

    Building Interface, but something bad happen.

--- JDBC1.0/src/dm/biz/AssetBiz.java
+++ JDBC1.0/src/dm/biz/AssetBiz.java
@@ -5,17 +5,17 @@ import java.util.List;
 import dm.po.Asset;
 
 public interface AssetBiz {
-	// 1 增加学生
+	// 1 增加财产
 	public boolean add(Asset a);
 
-	// 2 删除学生
-	public boolean delete(String ano);
+	// 2 删除财产
+	public boolean delete(String Ano);
 
-	// 3 修改学生
+	// 3 修改财产
 	public boolean update(Asset a);
 
-	// 4根据学号查找学生
-	public Asset findById(String ano);
+	// 4根据学号查找财产
+	public Asset findById(String Ano);
 
 	// 5 查询所有
 	public List<Asset> findAll();
---
+++ JDBC1.0/src/dm/biz/BuildingBiz.java
@@ -0,0 +1,25 @@
+package dm.biz;
+
+import java.util.List;
+
+import dm.po.Building;
+
+public interface BuildingBiz {  // Why does have '?' in there?
+	// 1 增加宿舍楼
+	public boolean add(Building b);
+
+	// 2 删除宿舍楼
+	public boolean delete(int Bno);
+
+	// 3 修改宿舍楼
+	public boolean update(Building b);
+
+	// 4根据学号查找宿舍楼
+	public Building findById(int Bno);
+
+	// 5 查询所有
+	public List<Building> findAll();
+
+	// 6 模糊查询
+	public List<Building> findByCondition(String condition);
+}
