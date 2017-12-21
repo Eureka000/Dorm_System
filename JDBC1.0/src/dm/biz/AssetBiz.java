0df8db487a022e8a9574f8689d02787e49fc60a6
zqy <greens_zhang@139.com>
2017-12-21 09:19:07 +0800

    Asset Interface and it's 实现.
    Asset Interface and it's 实现.

---
+++ JDBC1.0/src/dm/biz/AssetBiz.java
@@ -0,0 +1,26 @@
+package dm.biz;
+
+import java.util.List;
+
+import dm.po.Asset;
+
+public interface AssetBiz {
+	// 1 增加学生
+	public boolean add(Asset a);
+
+	// 2 删除学生
+	public boolean delete(String sno);
+
+	// 3 修改学生
+	public boolean update(Asset s);
+
+	// 4根据学号查找学生
+	public Asset findById(String sno);
+
+	// 5 查询所有
+	public List<Asset> findAll();
+
+	// 6 模糊查询
+	public List<Asset> findByCondition(String condition);
+
+}
