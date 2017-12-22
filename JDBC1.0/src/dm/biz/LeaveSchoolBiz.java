045847e7a384c9b8d1c01f170a0f0d3f48f2eb44
MoeMoeMortimer <youremail>
2017-12-22 10:55:09 +0800

    bizImpl and util

--- JDBC1.0/src/dm/biz/LeaveSchoolBiz.java
+++ JDBC1.0/src/dm/biz/LeaveSchoolBiz.java
@@ -1,20 +1,25 @@
 package dm.biz;
 
+import java.util.Date;
 import java.util.List;
 
-import dm.po.*;
+import dm.po.Dormitory;
+import dm.po.LeaveSchool;
 
 public interface LeaveSchoolBiz {
+	// 1 增加离校信息
 	public boolean add(LeaveSchool ls);
-
-	public boolean delete(LeaveSchool ls);
-
-	public boolean update(LeaveSchool ls);
-
-	public LeaveSchool findById(LeaveSchool ls);
-
+	
+	// 2 删除离校信息
+	public boolean delete(String dno, Date Sltime, Date Sreturn);
+	
+	
+	// 4根据学号查找某学生的离校信息
+	public LeaveSchool findById(String sno);
+	
+	// 5 查询所有
 	public List<LeaveSchool> findAll();
-
+	
+	// 6 模糊查询
 	public List<LeaveSchool> findByCondition(String condition);
-
 }
--- JDBC1.0/src/dm/biz/MailBiz.java
+++ JDBC1.0/src/dm/biz/MailBiz.java
@@ -2,19 +2,27 @@ package dm.biz;
 
 import java.util.List;
 
-import dm.po.*;
+import dm.po.Mail;
 
 public interface MailBiz {
+	// 1 增加快件
 	public boolean add(Mail m);
-
-	public boolean delete(Mail m);
-
+	
+	// 2 删除快件
+	public boolean delete(String mno);
+	
+	// 3 修改快件
 	public boolean update(Mail m);
-
-	public Mail findById(Mail m);
-
+	
+	// 4根据学号查找快件
+	public Mail findBySid(String sno);
+	
+	// 4根据快件号查找快件
+	public Mail findByMid(String mno);
+	
+	// 5 查询所有
 	public List<Mail> findAll();
-
+	
+	// 6 模糊查询
 	public List<Mail> findByCondition(String condition);
-
 }
